ff991941e7dd244c56873060ad91f99801cc52d5
JasonTJH <85621110+JasonTJH@users.noreply.github.com>
2021-06-16 22:42:38 +0800

    Update __init__.py
    Amended DB URL & SECRET KEY from hard coded string to reference config.yaml

--- ecommerce/__init__.py
+++ ecommerce/__init__.py
@@ -12,8 +12,6 @@ import yaml
 ######## Enable this for debugging #########
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://shend:root@localhost/ecommerce'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.secret_key = 'random string'
@@ -23,6 +21,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ######### Required in Case of firing complex queries without ORM #########
 db2 = yaml.load(open('config.yaml'))
+app.config['SECRET_KEY'] = db2['secret_key']
+app.config['SQLALCHEMY_DATABASE_URI'] = db2['mysql_url']
 app.config['MYSQL_HOST'] = db2['mysql_host']
 app.config['MYSQL_USER'] = db2['mysql_user']
 app.config['MYSQL_PASSWORD'] = db2['mysql_password']
